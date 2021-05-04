5d992f963faf3b7af4814339d97b13449b02ff54
Nimit Patel <np9@umbc.edu>
2021-05-04 17:05:10 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+      <h1>nimitpatel.me</h1>
+      <ul>
+          <li>Work Experience</li>
+          <li>Education</li>
+          <li>Projects</li>
+          <li>Resume</li>
+      </ul>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
